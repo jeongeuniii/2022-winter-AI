ec1046e35d6d0ddb529718630c839dab0950b1c8
jeongeuniii <mjn20011013@gmail.com>
2022-12-27 20:45:20 +0900

    DAY2
    코드 수정 완료

--- python/12_27.py
+++ python/12_27.py
@@ -3,17 +3,16 @@ print("="*8, "20223175 문정은", "="*8)
 print("="*8, "2일차 과제 결과", "="*8)
 print("="*33)
 
-num = int(input('몇단까지 출력할까요?(2~9단, 나머지 정수 값 입력시 종료됨)'))
 while True:
+    num = int(input('몇단까지 출력할까요?(2~9단, 나머지 정수 값 입력시 종료됨)'))
     if 2 <= num <=9:
         for j in range(1,10):
             for i in range(2,num+1):
                 print(i,'X',j,'=',i*j, end='\t')
             print()
-
-        break
+        continue
 
     else:
         print('이용해 주셔서 감사합니다.')
         print('개발자 : 문정은(20223175)')
-
+        break
