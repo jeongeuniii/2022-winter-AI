f279d1829df3c8c7f2cade22f4a10a8f908724fb
Jeongeun <110384223+jeongeuniii@users.noreply.github.com>
2022-12-28 11:04:34 +0900

    Update 12_27.py
    입력 수정

--- python/12_27.py
+++ python/12_27.py
@@ -4,13 +4,13 @@ print("="*8, "2일차 과제 결과", "="*8)
 print("="*33)
 
 while True:
-    num = int(input('몇단까지 출력할까요?(2~9단, 나머지 정수 값 입력시 종료됨)'))
+    print('몇단까지 출력할까요?(2~9단, 나머지 정수 값 입력시 종료됨)')
+    num = int(input('>>'))
     if 2 <= num <= 9:
         for j in range(1, 10):
             for i in range(2, num+1):
                 print(i, 'X', j, '=', i*j, end='\t')
             print()
-        continue
 
     else:
         print('이용해 주셔서 감사합니다.')
