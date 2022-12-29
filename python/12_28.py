4fc5669c115d84990f0de25f97b239eb924949b9
jeongeuniii <mjn20011013@gmail.com>
2022-12-29 11:02:51 +0900

    입력받을 num 범위 지정

--- python/12_28.py
+++ python/12_28.py
@@ -6,31 +6,54 @@ import numpy as np
 def Insert(student):
     num = int(input('학번 : '))
 
-    #학번이 딕셔너리 내에 없을경우,
-    if (num not in student) == True :
+    if (0 < num < 10):
 
-        kor =  int(input('%d의 국어 점수 : '%num))
-        math =  int(input('%d의 수학 점수 : '%num))
-        eng = int(input('%d의 영어 점수 : '%num))
-        save = input('%d의 점수를 저장하시겠습니까?'%num)
+        # 학번이 딕셔너리 내에 없을경우,
+        if (num not in student) == True:
 
-        if save == 'y' :
-            score = kor+eng+math
-            avg = round(score/3, 3)
-            print('학생(%d)의 점수 합계는 %d점, 평균은 %d입니다.'%(num,score,avg))
-            
-            #딕셔너리 Key = Value
-            student[num] = [kor, eng, math, score, avg]
-            person = student[num]
-            return student
-            
-        elif save == 'n' :
-            print('학생(%d)의 정보가 입력되지 않습니다.'%num)
+            kor = int(input('%d의 국어 점수 : ' % num))
+            math = int(input('%d의 수학 점수 : ' % num))
+            eng = int(input('%d의 영어 점수 : ' % num))
+            save = input('%d의 점수를 저장하시겠습니까?' % num)
+
+            if save == 'y':
+                score = kor+eng+math
+                avg = round(score/3, 3)
+                print('학생(%d)의 점수 합계는 %d점, 평균은 %d입니다.' % (num, score, avg))
+
+                if 90 <= avg:
+                    grade = 'A'
+
+                elif 80 <= avg < 90:
+                    grade = 'B'
+
+                elif 70 <= avg < 80:
+                    grade = 'C'
+
+                elif 60 <= avg < 70:
+                    grade = 'D'
+
+                else:
+                    grade = 'F'
+
+                # 딕셔너리 Key = Value
+                student[num] = [kor, eng, math, score, avg, grade]
+                person = student[num]
+                return student
+
+            elif save == 'n':
+                print('학생(%d)의 정보가 입력되지 않습니다.' % num)
+                return main()
+
+            else :
+                return main()
+
+        # 학번이 중복일 경우
+        else :
+            print('%d은(는) 성적이 존재하는 학생입니다')
             return main()
-
-    #학번이 중복일 경우
     else :
-        print('%d은(는) 성적이 존재하는 학생입니다')
+        print(num,'은 입력할 수 없습니다.')
         return main()
 
 # ----- 성적 현황 -----
@@ -38,12 +61,11 @@ def View(student):
     print("="*37)
     print('학번 : [국어, 영어, 수학, 총점, 평균]')
 
-    
     # 딕셔너리 key, value 출력하기(item)
     for key, value in student.items():
-        print(key,':', value)
+        print(key, ':', value)
     print("="*37)
-        
+
     return student
 
 def main():
@@ -57,7 +79,7 @@ def main():
     print('1. 성적입력')
     print('2. 성적현황')
     print('3. 종료')
-    print('='*33)
+    print('='*37)
 
     while True:
         select = int(input(">>"))
@@ -70,13 +92,13 @@ def main():
             student = Insert(student)
 
         # ----- 성적 현황 -----
-        elif select == 2 :
+        elif select == 2:
             print('='*37)
             print('학생 성적 현황')
             print('='*37)
             student = View(student)
-            
-        elif select == 3 :
+
+        elif select == 3:
             print('(프로그램 종료 - 개발자 : 20223175 문정은)')
             break
 
